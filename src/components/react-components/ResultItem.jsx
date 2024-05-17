58f4466fb6994706b42a8041aced4a03389b4aad
SDavila18023 <sdavilaa@unbosque.edu.co>
2024-05-17 17:14:34 -0500

    copies agregados e imagenes

--- src/components/react-components/ResultItem.jsx
+++ src/components/react-components/ResultItem.jsx
@@ -6,11 +6,6 @@ const ResultItem = ({ project }) =>
         <div className="result-item">
             <img src={project.image} alt={project.name} />
             <h3>{project.name}</h3>
-            <div className="result-item-features">
-                {project.features.map((feature, index) => (
-                    <span key={index}>{feature.name}</span>
-                ))}
-            </div>
             <p>{project.description}</p>
             <button onClick={() => window.location.href = '/projects'}>Descubre Más</button>
         </div>
